f54d5db832f853e685665ae73c9ec9d050b5a385
Dennis Moes <dennismoes@me.com>
2023-03-11 08:36:28 +0100

    Tried adding flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{ts,tsx}',
+    './public/**/*.html',
+    './node_modules/flowbite-react/**/*.js',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
