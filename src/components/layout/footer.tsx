ec3b3f4e07385af95a9c609e3baa4f5241cb6c4b
Dennis Moes <dennismoes@me.com>
2023-03-12 16:57:19 +0100

    Reworked the navbar and footer to add own branding

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,11 +4,7 @@ export default function Footer() {
   return (
     <footer className="p-4 bg-white md:flex md:items-center md:justify-between md:p-6">
       <span className="text-sm text-gray-500 sm:text-center">
-        © {today.getFullYear()}{' '}
-        <a href="https://flowbite.com/" className="hover:underline">
-          Flowbite™
-        </a>
-        . All Rights Reserved.
+        © {today.getFullYear()} Velocity Sales™. All Rights Reserved.
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
         <li>
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,7 +1,9 @@
 import { useWallet } from '@/context/WalletContext'
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import wheel from '../../images/car-wheel.png'
 
 const selectedStyling =
   'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0'
@@ -22,16 +24,12 @@ export default function Navbar() {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-6 mr-3 sm:h-9"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-xl font-semibold whitespace-nowrap">
-            Flowbite
+        <Link href="/" className="flex items-center">
+          <Image alt="Website logo" src={wheel} width={50} height={50} />
+          <span className="ml-2 self-center text-xl font-semibold whitespace-nowrap">
+            Velocity Sales
           </span>
-        </a>
+        </Link>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
