cf491908fce1f12996b74005de62c11b0862888c
Dennis Moes <dennismoes@me.com>
2023-03-12 17:14:27 +0100

    Reworked the landing page to be as simple as possible

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import useMetaMask from '../hooks/useMetamask';
+import { useWallet } from '@/context/WalletContext'
+import Head from 'next/head'
 
 export default function Home() {
-  const metamask = useMetaMask();
+  const { connectWallet } = useWallet()
 
   return (
     <>
@@ -13,8 +13,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Home</h1>
-        <button>Log in with metamask</button>
+        <div className="mt-20 flex flex-col items-center justify-center">
+          <div className="flex flex-row items-center justify-center bg-white p-4 rounded-md shadow-lg space-x-2">
+            <div className="max-w-2xl relative">
+              <p className="font-medium text-4xl">
+                Discover your dream car at our second-hand dealership
+              </p>
+              <p className="mt-2 mb-4">
+                Ready to take your driving experience to the next level? Our
+                dealership platform has a selection of high-quality used cars to
+                choose from. All at unbeatable prices. With us you can hit the
+                road in style and you&apos;ll will feel confident and excited
+                about your purchase. Get started today and start your adventure!
+              </p>
+              <button
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+                onClick={connectWallet}
+              >
+                Get started
+              </button>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   )
