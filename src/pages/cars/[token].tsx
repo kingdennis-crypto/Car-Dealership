d778b7865e3de91bf7b6603d2174268dd76a5802
Dennis Moes <dennismoes@me.com>
2023-03-16 10:46:24 +0100

    Added a edit menu

--- src/pages/cars/[token].tsx
+++ src/pages/cars/[token].tsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react'
 import { useWallet } from '@/context/WalletContext'
 import { useRouter } from 'next/router'
 import { ethers } from 'ethers'
-import { toast } from 'react-toastify'
 import { ThirdwebStorage } from '@thirdweb-dev/storage'
+import dateformat from 'dateformat'
 
 import Head from 'next/head'
 import Image from 'next/image'
@@ -14,11 +14,14 @@ import Car from '@/models/car'
 import Modal from '@/components/layout/modal'
 import InputField from '@/components/input/InputField'
 import CarImage from '@/components/input/CarImage'
+import Mileage from '@/models/mileage'
 
-// TODO: Work on the mileage editor, by making sure the new cannot be lower than the old and that the mileage will be saved so that it can be called later
-// TODO: Only the owner can indicate if the car is for sale and indicate an asking price, he can adjust both later
+type EditMenuDetail = {
+  type: number
+  title: string
+  isOpen: boolean
+}
 
-// TODO: Maintenance records and service history
 export default function LicensePlate() {
   const storage = new ThirdwebStorage()
   const router = useRouter()
@@ -28,18 +31,35 @@ export default function LicensePlate() {
     useWallet()
 
   const [car, setCar] = useState<Car>()
-  const [isOpen, setIsOpen] = useState<boolean>(false)
   const [mileage, setMileage] = useState<number>(0)
   const [images, setImages] = useState<string[]>([])
   const [selectedImage, setSelectedImage] = useState<string>('')
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [mileageHistory, setMileageHistory] = useState<Mileage[]>([])
+
+  const [editMenu, setEditMenu] = useState<EditMenuDetail>({
+    title: 'None',
+    type: 0,
+    isOpen: false,
+  })
 
   async function getCarDetails() {
-    const _car: Car = Car.fromArray(
-      await callContractFunction('getCarByToken', token)
-    )
+    const _carData = await callContractFunction('getCarByToken', token)
+
+    if (!_carData) {
+      return alert('Error fetching car')
+    }
+
+    const _car: Car = Car.fromArray(_carData)
 
     const _data = await storage.downloadJSON(_car.metadataUri)
+
+    if (!_data) {
+      return alert('Error downloading metadata')
+    }
+
     const _images: string[] = Object.values(_data.images)
+
     setImages(_images)
     setSelectedImage(_images[0])
 
@@ -53,51 +73,69 @@ export default function LicensePlate() {
     }
 
     getCarDetails()
+    getMileageHistory()
     // eslint-disable-next-line
   }, [router])
 
   // TODO: On buying the car reload the page or update the details
   async function buyCar() {
-    console.log('Buying the car')
     const _priceInEther = ethers.utils.parseEther(car?.price.toString() || '0')
-    const _data: any = await callContractFunctionWithEthers(
-      'buyCar',
-      _priceInEther,
+    await callContractFunctionWithEthers('buyCar', _priceInEther, token)
+  }
+
+  async function retrieveCar() {
+    const _data: any = await callContractFunction('retrieveCar', token)
+  }
+
+  async function cancelCarOrder() {
+    const _data: any = await callContractFunction('cancelCarOrder', token)
+  }
+
+  async function getMileageHistory() {
+    const _data: Mileage[] = await callContractFunction(
+      'getCarMileageHistory',
       token
+    )
+
+    const _mileage = _data.map((item) => Mileage.fromArray(item))
+    setMileageHistory(_mileage)
+  }
+
+  async function addMileageToHistory() {
+    if (mileage <= car?.mileage) {
+      return alert('New car mileage is not high enough')
+    }
+
+    const now = new Date()
+    const _mileage = new Mileage(parseInt(car?.tokenId), mileage, now)
+
+    const _data: any = await callContractFunction(
+      'addMileageReport',
+      token,
+      mileage,
+      now.valueOf()
+    )
+
+    car!.mileage = mileage
+    setMileageHistory([...mileageHistory, _mileage])
+  }
+
+  async function updatePrice() {
+    const _data: any = await callContractFunction(
+      'changeCarPrice',
+      token,
+      car?.price
     )
     console.log(_data)
   }
 
-  async function retrieveCar() {
-    console.log('Retrieving the car')
-    const _data: any = await callContractFunction('retrieveCar', token)
+  async function updateAvailability() {
+    const _data: any = await callContractFunction(
+      'changeCarAvailability',
+      token,
+      car?.forSale
+    )
     console.log(_data)
-  }
-
-  async function cancelCarOrder() {
-    console.log('Canceling car order')
-    const _data: any = await callContractFunction('cancelCarOrder', token)
-    console.log(_data)
-  }
-
-  async function editMileage() {
-    console.log('EDITING MILEAGE')
-    setIsOpen(true)
-  }
-
-  async function updateCarMileage() {
-    const newMileageIsHigher = car?.mileage < mileage
-    console.log(car?.mileage, mileage, newMileageIsHigher)
-
-    // TODO: Swap with toastify
-    if (!newMileageIsHigher) {
-      return alert('The new mileage is not higher')
-    }
-
-    callContractFunction('changeMileage', token, mileage)
-    car!.mileage = mileage
-    // const _car: Car = await callContractFunction('changeMileage', token, mileage);
-    // setCar(_car)
   }
 
   return (
@@ -145,35 +183,103 @@ export default function LicensePlate() {
                 <p>{car?.price} ETH</p>
               </div>
             </div>
-            <div>
-              {car?.owner === address && (
+            <button
+              onClick={() => setIsEditing(!isEditing)}
+              className="p-2 border-2 hover:bg-gray-100 rounded-md aspect-square"
+            >
+              <svg
+                aria-hidden="true"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-12 h-12"
+              >
+                <path
+                  d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <hr className="my-4" />
+          {isEditing && (
+            <>
+              <div className="flex flex-row gap-2">
                 <button
-                  type="button"
-                  onClick={editMileage}
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-4 text-center inline-flex items-center"
+                  onClick={() =>
+                    setEditMenu({
+                      title: 'Add mileage report',
+                      type: 1,
+                      isOpen: true,
+                    })
+                  }
+                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
                 >
-                  <svg
-                    aria-hidden="true"
-                    fill="none"
-                    className="w-5 h-5 mr-1"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>
-                  </svg>
-                  <span className="sr-only">Icon description</span>
-                  Edit Mileage
+                  Add Mileage
                 </button>
-              )}
+                <button
+                  onClick={() =>
+                    setEditMenu({
+                      title: 'Change price',
+                      type: 2,
+                      isOpen: true,
+                    })
+                  }
+                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+                >
+                  Change Price
+                </button>
+                <button
+                  onClick={() =>
+                    setEditMenu({
+                      title: 'Change availability',
+                      type: 3,
+                      isOpen: true,
+                    })
+                  }
+                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+                >
+                  Change Availability
+                </button>
+              </div>
+              <hr className="my-4" />
+            </>
+          )}
+
+          <div>
+            <div className="relative overflow-x-auto overflow-hidden rounded-md">
+              <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    <th scope="col" className="px-6 py-3 w-1/2">
+                      Date
+                    </th>
+                    <th scope="col" className="px-6 py-3 w-1/2">
+                      Mileage
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {mileageHistory.map((item, index) => (
+                    <tr key={index} className="bg-white border-b">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        {dateformat(item.date)}
+                      </th>
+                      <td className="px-6 py-4">{item.mileage}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
           <hr className="my-4" />
+
           {car?.owner !== address && (
             <div>
               {car?.sold ? (
@@ -259,35 +365,140 @@ export default function LicensePlate() {
               )}
             </div>
           )}
-        </div>
 
-        <Modal
-          title="Edit Mileage"
-          isOpen={isOpen}
-          onClose={() => setIsOpen(false)}
-          footer={
-            <>
-              <button
-                type="button"
-                className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                onClick={updateCarMileage}
-              >
-                Update Mileage
-              </button>
-            </>
-          }
-        >
-          <div>
-            <InputField
-              id="car_mileage"
-              label="Mileage"
-              type="number"
-              value={mileage}
-              // onChange={(e) => setCar({ ...car, mileage: e })}
-              onChange={(e) => setMileage(parseInt(e))}
-            />
-          </div>
-        </Modal>
+          <Modal
+            title={editMenu.title}
+            isOpen={editMenu.isOpen}
+            onClose={() => setEditMenu({ ...editMenu, isOpen: false })}
+          >
+            <hr className="my-2" />
+            <div>
+              {editMenu.type === 1 && (
+                <>
+                  <InputField
+                    id="addMileage"
+                    type="number"
+                    label="Add Mileage"
+                    placeholder="Mileage"
+                    value={mileage}
+                    onChange={(e) => setMileage(parseInt(e))}
+                  />
+                  <button
+                    onClick={addMileageToHistory}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2"
+                  >
+                    Add Mileage
+                  </button>
+                </>
+              )}
+              {editMenu.type === 2 && (
+                <>
+                  <InputField
+                    id="changePrice"
+                    type="number"
+                    label="Change price"
+                    placeholder="price"
+                    value={car?.price}
+                    onChange={(e) => setCar({ ...car!, price: parseInt(e) })}
+                  />
+                  <button
+                    onClick={updatePrice}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2"
+                  >
+                    Change Price
+                  </button>
+                </>
+              )}
+              {editMenu.type === 3 && (
+                <>
+                  <ul className="flex flex-col w-full gap-2">
+                    <li>
+                      <input
+                        type="radio"
+                        id="hosting-small"
+                        name="hosting"
+                        checked={Number(car?.forSale) === 0}
+                        value={Number(car?.forSale)}
+                        onChange={() => setCar({ ...car!, forSale: false })}
+                        className="hidden peer"
+                        required
+                      />
+                      <label
+                        htmlFor="hosting-small"
+                        className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100"
+                      >
+                        <div className="block">
+                          <div className="w-full text-lg font-semibold">
+                            Not for sale
+                          </div>
+                          <div className="w-full">
+                            If you don&apos;t wan&apos;t your car to be buyable
+                          </div>
+                        </div>
+                        <svg
+                          aria-hidden="true"
+                          className="w-6 h-6 ml-3"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                          ></path>
+                        </svg>
+                      </label>
+                    </li>
+                    <li>
+                      <input
+                        type="radio"
+                        id="hosting-big"
+                        name="hosting"
+                        className="hidden peer"
+                        checked={Number(car?.forSale) === 1}
+                        value={Number(car?.forSale)}
+                        onChange={() => setCar({ ...car!, forSale: true })}
+                      />
+                      <label
+                        htmlFor="hosting-big"
+                        className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100"
+                      >
+                        <div className="block">
+                          <div className="w-full text-lg font-semibold">
+                            For sale
+                          </div>
+                          <div className="w-full">
+                            If you want to sell your car
+                          </div>
+                        </div>
+                        <svg
+                          aria-hidden="true"
+                          className="w-6 h-6 ml-3"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                          ></path>
+                        </svg>
+                      </label>
+                    </li>
+                  </ul>
+                  <button
+                    onClick={updateAvailability}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2"
+                  >
+                    Update availability
+                  </button>
+                </>
+              )}
+            </div>
+          </Modal>
+        </div>
       </div>
     </>
   )
